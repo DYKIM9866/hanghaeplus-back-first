90d20576b9ff10dd492f8fdc1fe0cab3e611f03d
daeyeong <kimdae0@wavus.co.kr>
2024-03-22 15:55:53 +0900

    Controller 수정

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RequestMapping("/point")
@@ -20,16 +21,15 @@ public class PointController {
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
      */
     @GetMapping("{id}")
-    public UserPoint point(@PathVariable("id") int id) throws InterruptedException {
-        Long s = Integer.toUnsignedLong(id);
-        return pointService.selectPointById(s);
+    public UserPoint point(@PathVariable("id") Long id) throws InterruptedException {
+        return pointService.selectPointById(id);
     }
 
     /**
      * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
      */
     @GetMapping("{id}/histories")
-    public List<PointHistory> history(@PathVariable Long id) {
+    public List<PointHistory> history(@PathVariable("id") Long id) {
         return pointService.pointHistories(id);
     }
 
@@ -37,15 +37,15 @@ public class PointController {
      * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
      */
     @PatchMapping("{id}/charge")
-    public UserPoint charge(@PathVariable Long id, @RequestBody Long amount) throws InterruptedException {
-        return pointService.charge(id,amount);
+    public UserPoint charge(@PathVariable("id") Long id, @RequestBody Map<String,Long> amount) throws InterruptedException {
+        return pointService.charge(id,amount.get("amount"));
     }
 
     /**
      * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
      */
     @PatchMapping("{id}/use")
-    public UserPoint use(@PathVariable Long id, @RequestBody Long amount) throws InterruptedException {
-        return pointService.usePoint(id,amount);
+    public UserPoint use(@PathVariable("id") Long id, @RequestBody Map<String,Long> amount) throws InterruptedException {
+        return pointService.usePoint(id,amount.get("amount"));
     }
 }
